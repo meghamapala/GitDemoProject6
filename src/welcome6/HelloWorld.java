dd7b189e9e644fbb578487e13c400accdebd6962
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-02-26 12:03:30 -0500

    second commit

--- src/welcome6/HelloWorld.java
+++ src/welcome6/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Helloworld 6  !");
+		
+		System.out.println("People  can use solar power to save energy  !");
 
 	}
 
